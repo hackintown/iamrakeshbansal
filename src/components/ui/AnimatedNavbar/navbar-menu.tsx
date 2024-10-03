98d1b39776665f7233bcc8cb37b4af3eeffe3574
Manoj Kumar <manojkumarcse432@gmail.com>
2024-10-03 18:16:17 +0530

    home-page-fixed

--- src/components/ui/AnimatedNavbar/navbar-menu.tsx
+++ src/components/ui/AnimatedNavbar/navbar-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import Image from "next/image";
 
 const transition = {
@@ -110,11 +110,14 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({
+  children,
+  ...rest
+}: LinkProps & { children: React.ReactNode }) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black"
     >
       {children}
     </Link>
